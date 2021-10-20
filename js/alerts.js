665bd74b7d00e2602767a2cf020b812cbbbff0dc
FernandoFeN <fernandofenosv@gmail.com>
2021-10-20 00:02:33 -0500

    first commit

---
+++ js/alerts.js
@@ -0,0 +1,39 @@
+function knowMore() {
+  swal(`Meraki es una palabra que proviene de la legua griega y que nos expresa más una 
+  idea compleja que un concepto concreto.
+  En algunos diccionarios se intenta traducir como; curiosidad, capricho, asombro, 
+  interés o afición e incluso, algunas veces, aparece como cuidado, elegancia o moda. 
+  Aun que todas estas definiciones no andan del todo equivocadas, la verdad es que esta 
+  palabra no tiene una traducción simple a otros idiomas. Sin embargo, si queremos darle 
+  una interpretación cercana a la idea que nos transmite, podemos decir que Meraki significa
+  dedicación, alma, creatividad, pasión y amor por algo. La esencia de ti mismo reflejada en 
+  lo que haces.`);
+}
+
+function values() {
+  swal(`Honestidad: la transparencia, sinceridad y franqueza, ya que lo primero que queremos concientizar en la empresa es la confianza y credibilidad 
+  Puntualidad: ser responsables con lo que se está vendiendo a los clientes
+  Convicción: por la elaboración del producto que se va realizar
+  Respeto: respeto por las personas quienes lo van estar elaborando como en el empaquetado
+  Empatía: hacia las personas que están realizando la producción y la entrega del producto`);
+}
+
+function vision() {
+  swal(`Nuestra misión como Meraki barquillo, es que 
+  puedas conocer nuestra variedad de sabores, como microempresa, ayudando asi también 
+  reciclando envases de plástico, asimismo ya que hay poca variedad en este, negocio, 
+  también queriendo innovar una nueva manera de disfrutar el producto
+    Visión
+    Nuestra visión es que en un futuro no solo podamos venderlo 
+    de manera local, ya que le producto se dé a conocer si no que también poder
+    negociar con grandes franquicias como la michoacana nuestro producto y empiece
+    a ser más conocido en el mercado para que igualmente puedan tener un mayor impacto 
+    de no solo quedarse con los sabores conocidos`);
+}
+
+function whoWeAre() {
+  swal(`Nuestra filosofía, es principalmente crear una nueva variedad de 
+  barquillos como los sabores ya mencionados, queremos crear no solo la confianza de que 
+  comas el barquillo, si no ofrecer la experiencia, del barquillo, no solo
+  porque es saludable si no porque, las variaciones que se ofrecen son del agrado del publico`);
+}
